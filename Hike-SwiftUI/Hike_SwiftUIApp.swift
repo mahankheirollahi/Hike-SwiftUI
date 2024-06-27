da2a9ed38cfd51491be5963fe878bac808347eab
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-27 10:44:59 +0300

    Initial Commit

---
+++ Hike-SwiftUI/Hike_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Hike_SwiftUIApp.swift
+//  Hike-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/27/24.
+//
+
+import SwiftUI
+
+@main
+struct Hike_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
