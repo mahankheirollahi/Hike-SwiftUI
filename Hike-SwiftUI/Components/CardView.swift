19b948b75c13c2039e760fff535ad9a8b124f0a1
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-27 23:08:58 +0300

    Add card view

--- Hike-SwiftUI/Components/CardView.swift
+++ Hike-SwiftUI/Components/CardView.swift
@@ -51,11 +51,27 @@ struct CardView: View {
                             .resizable()
                             .scaledToFit()
                     }
+                Button{
+                    print("Button pressed")
+                }label:{
+                    Text("Explore More")
+                        .font(.title2)
+                        .fontWeight(.heavy)
+                        .foregroundStyle(LinearGradient(colors: [.customGreenLight,.customGreenMedium], startPoint: .top, endPoint: .bottom)
+                        )
+                        .shadow(color: .black.opacity(0.25),
+                                radius: 0.25,x: 1,y: 2)
+                }
+                .buttonStyle(GradientButton())
+               
+               
+                
                     
                 }
             }   .frame(width: 320,height: 570)
             
             // MARK: - FOOTER
+       
         }//: CARD
      
     }
