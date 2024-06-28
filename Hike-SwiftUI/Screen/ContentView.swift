9b8e4b8f7a1faa292b2607ae6ed4fa60e3226d88
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-06-28 23:29:10 +0300

    Change content view path

---
+++ Hike-SwiftUI/Screen/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  Hike-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 6/27/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        CardView()
+    }
+}
+
+#Preview {
+    ContentView()
+}
